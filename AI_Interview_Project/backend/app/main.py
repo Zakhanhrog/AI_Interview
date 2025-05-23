ee8d46d28107d2c03239fa4fd949f852d5cabc31
Ngo Gia Khanh <ngogiakhanh@MacBook-Pro-cua-Ngo.local>
2025-05-23 11:09:43 +0700

    Add link front web

--- AI_Interview_Project/backend/app/main.py
+++ AI_Interview_Project/backend/app/main.py
@@ -62,7 +62,10 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "http://localhost:5173",
+        "https://ai-interview-silk-one.vercel.app/"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
